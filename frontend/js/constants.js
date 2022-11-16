c1e61d099d5258ad672e00cd19fdca82f0df6837
inknovate <118134877+inknovate@users.noreply.github.com>
2022-11-16 22:28:13 +0800

    front end update

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'goerli'; // goerli, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the MiceFriends NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The MiceFriends NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'Mice Friends NFT is a collectionn of 5,000 randomly generated Unique images. This project is created to lift everyones situation upon sold out a percentage will be donated to a selected group. Treasury will be use to sweep floor depending on what project the holders will be voted on.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
