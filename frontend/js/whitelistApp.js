07cdc18f33e242dba74e6647d9f7c6328c175bf3
inknovate <118134877+inknovate@users.noreply.github.com>
2022-11-30 20:15:43 +0800

    update for whitelist button

--- frontend/js/whitelistApp.js
+++ frontend/js/whitelistApp.js
@@ -1,10 +1,13 @@
 
   const scriptURL = 'https://script.google.com/macros/s/AKfycbzqMLV0wmfQKqqZhiQtcbtuPa9pZGuAwg9Pd-wuqfCbRf8FFjktV4Wg_r9Vgw6mmKcL/exec'
   const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
 
   form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
+      .then(response => {
+        msg.innerHTML = "Congratulations Your Address has been Whitelisted!"
+      })
       .catch(error => console.error('Error!', error.message))
   })
